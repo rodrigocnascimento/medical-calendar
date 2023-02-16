7072c886b5f4a9f8ac990cbb82c319af446bcae8
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2023-02-16 12:30:49 -0300

    feat (backend): initial backend NestJS configuration

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post, Get, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): any {
+    return this.appService.hello();
+  }
+}
