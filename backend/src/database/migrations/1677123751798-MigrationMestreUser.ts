b4446fe442d7ae9ce87be0f80f8a6403162264be
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2023-02-25 19:36:41 -0300

    feat[close #14](remove-plain-pass): forgot a file

--- backend/src/database/migrations/1677123751798-MigrationMestreUser.ts
+++ backend/src/database/migrations/1677123751798-MigrationMestreUser.ts
@@ -1,13 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
-
+import * as bcrypt from "bcrypt";
 export class MigrationMestreUser1677123751798 implements MigrationInterface {
   name = "MigrationMestreUser1677123751798";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    const hashedPassword = await bcrypt.hash("123123", 1_0);
+
     await queryRunner.query(`
             INSERT INTO users 
                 (id,name,email,"role","password",created_at,updated_at) 
-            VALUES ('8d184f2f-cac4-48e8-be05-8d0a5dbb3ba9','User Mestre','user_mestre@email.com','admin','123123','2023-02-23 11:27:55.87583','2023-02-23 11:27:55.87583');
+            VALUES ('8d184f2f-cac4-48e8-be05-8d0a5dbb3ba9','User Mestre','user_mestre@email.com','admin','${hashedPassword}','2023-02-23 11:27:55.87583','2023-02-23 11:27:55.87583');
         `);
 
     await queryRunner.query(`
